98c9a210a4825b4a956f9462de6db54a95decd13
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-07-22 09:14:56 -0700

    Update ONEMORE.c

--- DAY-01/ONEMORE.c
+++ DAY-01/ONEMORE.c
@@ -1,3 +1,8 @@
+/*Input Format :
+The first line of input will contain a single integer T, denoting the number of test cases.Each test case consists of one line of input, containing a single integer X — the amount of time from now at which Chef's exam starts.*/
+/*Output Format:
+For each test case, output on a new line the answer — YES if Chef will finish his episode before the exam starts, and NO otherwise.*/
+
 #include <stdio.h>
 
 int main(void) {
