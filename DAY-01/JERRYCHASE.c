d9877b5e2a010ed1f18d08cfe52bfff1f0cb9c25
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-07-22 09:12:45 -0700

    Update JERRYCHASE.c

--- DAY-01/JERRYCHASE.c
+++ DAY-01/JERRYCHASE.c
@@ -1,3 +1,9 @@
+/*Input Format:
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two space-separated integers X and Y — the speeds of Jerry and Tom respectively.*/
+/*Output Format:
+For each test case, output on a new line, YES, if Tom will be able to catch Jerry. Otherwise, output NO.*/
+
 #include <stdio.h>
 
 int main(void) {
