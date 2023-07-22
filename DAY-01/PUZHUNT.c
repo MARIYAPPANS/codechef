33e4be690a170457849afde9857dec94e5745ef2
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-07-22 09:16:33 -0700

    Update PUZHUNT.c

--- DAY-01/PUZHUNT.c
+++ DAY-01/PUZHUNT.c
@@ -1,3 +1,7 @@
+/*Input Format:
+The first and only line of input will contain a single integer N: the number of people present in Chef's team.*/
+/*Output Format
+Print the answer: Yes if Chef's team is eligible to participate, and No otherwise.*/
 #include <stdio.h>
 
 int main(void) {
