e37b5b2a5e3b64c7667c041f2a9eb56eb2dacae4
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-07-22 09:10:57 -0700

    Update CANDIVIDE.c

--- DAY-01/CANDIVIDE.c
+++ DAY-01/CANDIVIDE.c
@@ -1,3 +1,9 @@
+/*Input Format:
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of a single integer N - the number of candies.*/
+/*Output Format:
+For each test case, output YES, if we can distribute all the candies between the three friends equally. Otherwise output NO.*/
+
 #include <stdio.h>
 
 int main(void) {
