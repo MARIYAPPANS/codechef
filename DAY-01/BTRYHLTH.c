b5f12bf46be2196ed57550dd8d14d437a9c2f864
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-07-22 09:08:56 -0700

    Update BTRYHLTH.c

--- DAY-01/BTRYHLTH.c
+++ DAY-01/BTRYHLTH.c
@@ -1,3 +1,9 @@
+/*Input Format:
+The first line of input will contain a single integer T, denoting the number of test cases.
+The first and only line of each test case contains an integer X — the battery health.*/
+/*Output Format:
+For each test case, output on a new line, YES, if the battery is in optimal condition, and NO otherwise.*/
+
 #include <stdio.h>
 
 int main(void) {
