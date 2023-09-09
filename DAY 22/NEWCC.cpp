798ef7819d1a0a71f1a4a2e7311f57a189539d71
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2023-09-09 08:01:56 -0700

    Add files via upload

---
+++ DAY 22/NEWCC.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x<y){
+	    cout<<"Old"<<endl;
+	}
+	if(x>y){
+	    cout<<"New"<<endl;
+	}
+	if(x==y){
+	    cout<<"Same"<<endl;
+	}
+	
+	return 0;
+}
